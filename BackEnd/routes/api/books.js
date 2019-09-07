f279041a0991d150c007f644c12aca563d3c65cf
Phil Dugliss <phillipdugliss@gmail.com>
2019-09-07 13:19:04 -0500

    DB created

---
+++ BackEnd/routes/api/books.js
@@ -0,0 +1,5 @@
+const router = require("express").Router();
+const booksController = require("../../controllers/booksController");
+
+
+module.exports = router;
--- BackEnd/server.js
+++ BackEnd/server.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const path = require("path");
+const routes = require("./routes");
+const mongoose = require("mongoose");
 const PORT = process.env.PORT || 3001;
 const app = express();
 
@@ -12,9 +14,14 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
+app.use(routes)
+
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
+
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/Google-Books");
 
 
 app.listen(PORT, () => {
